51e579f0fe6dd293b01d67b8e7de28de83c093c4
Felipe Barbosa <felipe.csultimate@gmail.com>
2022-09-03 21:06:25 -0300

    feat: Adicionando teste de criação de usuário.

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,70 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserError } from "./CreateUserError";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+
+let createUserUseCase: CreateUserUseCase;
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+
+
+
+describe("Create User", () =>{
+
+
+    beforeEach(() => {
+        inMemoryUsersRepository = new InMemoryUsersRepository();
+        createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+    })
+
+    it("should be abler to create a new user", async () =>{
+
+        const user = {
+            name:"Felipe Barbosa",
+            email: "felipe@gmail.com",
+            password: "123452"
+        }
+
+      await  createUserUseCase.execute({
+           email: user.email,
+           name: user.name,
+           password: user.password
+        });
+
+        const userCreated =  await inMemoryUsersRepository.findByEmail(user.email);
+
+        console.log(userCreated);
+
+
+        expect(userCreated).toHaveProperty("id");
+        
+
+    });
+
+    it("should not be abler to create a new user with email exists", async () =>{
+
+       expect(async ()=> {
+
+        const user = {
+            name:"Felipe Barbosa",
+            email: "felipe@gmail.com",
+            password: "123452"
+        }
+
+        await  createUserUseCase.execute({
+           email: user.email,
+           name: user.name,
+           password: user.password
+        });
+
+        await  createUserUseCase.execute({
+            email: user.email,
+            name: user.name,
+            password: user.password
+         });
+       }).rejects.toBeInstanceOf(CreateUserError);
+
+       
+    });
+
+});
